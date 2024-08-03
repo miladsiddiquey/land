c8b3dd903ff80c6a87f2e9ed1a5de436e78bd2d2
Milad Siddiquey <miladsiddiquey@gmail.com>
2024-08-03 17:29:48 +0600

    update s

--- admin/pages/landPost/delate_post.php
+++ admin/pages/landPost/delate_post.php
@@ -1,11 +1,52 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>this is delate page post</h1>
-</body>
-</html>
+<?php
+include "../../config.php";
+$obj = new Database();
+
+$id = (int)$_GET['id'];
+if ($id > 0) {
+    
+    $obj->select('post_data', '*', null, "id='$id'", null, null);
+    $result = $obj->getResult();
+
+    if ($result && count($result) > 0) {
+        $row = $result[0];
+        $images = $row['images'];
+
+        $deleteQuery = "DELETE FROM post_data WHERE id='$id'";
+
+        // Delete the record from the database
+        $deleteResult = $obj->delete('post_data', "id='$id'");
+
+        if ($deleteResult) {
+            // Delete the image file from the server
+            if (file_exists("../../upload_images/".$images)) {
+                unlink("../../upload_images/".$images);
+            }
+            ?>
+            <script>
+                alert("Data deleted successfully");
+                window.open('http://localhost/land/admin/pages/landPost/list_post.php', '_self');
+            </script>
+            <?php
+        } else {
+            ?>
+            <script>
+                alert("Error deleting data. Please try again.");
+            </script>
+            <?php
+        }
+    } else {
+        ?>
+        <script>
+            alert("Record not found.");
+        </script>
+        <?php
+    }
+} else {
+    ?>
+    <script>
+        alert("Invalid ID.");
+    </script>
+    <?php
+}
+?>
